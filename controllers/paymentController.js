76f7e9f7ca561a5a52388e5731ad20268b75d2ec
Jitendra Paswan <107781622+jitendrapasi@users.noreply.github.com>
2023-04-27 00:59:56 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -40,7 +40,7 @@ export const paymentVerification = async (req, res) => {
     });
 
     res.redirect(
-      `https://server-virid-one.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
+      `https://razorpaymern.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
